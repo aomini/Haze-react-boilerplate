0a5b959edc5ea353bc0e2082fe4fa3a74977c731
aomini daiki <rakesh.shrestha@hazesoft.co>
2022-02-15 22:21:50 +0545

    additional module alias

--- config-overrides.js
+++ config-overrides.js
@@ -7,6 +7,12 @@ module.exports = function override(config, env) {
     ...config.resolve,
     alias: {
       '@/components': path.resolve(__dirname, './src/components'),
+      '@/features': path.resolve(__dirname, './src/features'),
+      '@/pages': path.resolve(__dirname, './src/pages'),
+      '@/hooks': path.resolve(__dirname, './src/hooks'),
+      '@/resources': path.resolve(__dirname, './src/resources'),
+      '@/types': path.resolve(__dirname, './src/types'),
+      '@/utils': path.resolve(__dirname, './src/utils'),
     },
   };
   return config;
